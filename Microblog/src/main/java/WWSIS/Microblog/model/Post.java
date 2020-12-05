f3fd69fb64b87ca08e7c011bc811dc204d4213b8
Daria Dziubałtowska <daria305@gmail.com>
2020-12-05 10:35:09 +0100

    Finish post mapping, add getters, setters

--- Microblog/src/main/java/WWSIS/Microblog/model/Post.java
+++ Microblog/src/main/java/WWSIS/Microblog/model/Post.java
@@ -2,22 +2,35 @@ package WWSIS.Microblog.model;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.persistence.ManyToOne;
+import javax.persistence.JoinColumn;
 
+@Entity
 @Table(name="post")
 public class Post {
 	@Id
-	@GeneratedValue
+	@GeneratedValue()
 	@Column(name="PostId")
 	private int postId;
+	
+	//TODO @OneToMany(mappedBy = "post") - add to user table
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name="userId")
 	@Column(name="UserId")
 	private int userId;
+	
 	@Column(name="PostText")
 	private String PostText;
+	
 	@Column(name="Public")
 	private Boolean isPublic;
+	
 	@Column(name="PostedDate")
 	private String date;
+	
+	
 }
