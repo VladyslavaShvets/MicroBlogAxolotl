6ff650e795e83521e1e54aeb5fec68049258c66a
Daria Dziubałtowska <daria305@gmail.com>
2020-12-05 10:57:47 +0100

    Correct xml Spring config file, ana small changes

--- Microblog/src/main/java/WWSIS/Microblog/model/Post.java
+++ Microblog/src/main/java/WWSIS/Microblog/model/Post.java
@@ -1,12 +1,12 @@
-package WWSIS.Microblog.model;
+package WWSIS.Microblog.model; 
+
+import java.sql.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
-import javax.persistence.ManyToOne;
 import javax.persistence.JoinColumn;
 
 @Entity
@@ -14,23 +14,38 @@ import javax.persistence.JoinColumn;
 public class Post {
 	@Id
 	@GeneratedValue()
-	@Column(name="PostId")
+	@Column(name="PostId", nullable=false)
 	private int postId;
+	public int getPostId() {
+		return postId;
+	}
 	
-	//TODO @OneToMany(mappedBy = "post") - add to user table
-	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name="userId")
-	@Column(name="UserId")
+	@Column(name="UserId", nullable=false)
 	private int userId;
 	
-	@Column(name="PostText")
-	private String PostText;
+
+	@Column(name="PostText", nullable=false, length=300)
+	private String postText;
+	public String getPostText() {
+		return postText;
+	}
+	public void setPostText(String text) {
+		this.postText = text;
+	}
 	
-	@Column(name="Public")
+	@Column(name="Public", nullable=false)
 	private Boolean isPublic;
+	public Boolean getIsPublic() {
+		return isPublic;
+	}
+	public void setIsPublic(Boolean isPublic) {
+		this.isPublic = isPublic;
+	}
 	
-	@Column(name="PostedDate")
-	private String date;
-	
-	
+	@Column(name="PostedDate", nullable=false)
+	private Date date;
+	public Date getDate() {
+		return this.date;
+	}
 }
