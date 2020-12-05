9c17981e222b908712d574b29a15002b9b36825e
Vladyslava Shvets <vladywyrm@gmail.com>
2020-12-05 11:10:01 +0100

    add getter and setter to User class

--- Microblog/src/main/java/WWSIS/Microblog/model/User.java
+++ Microblog/src/main/java/WWSIS/Microblog/model/User.java
@@ -15,14 +15,59 @@ public class User{
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private int userId;
-	
+
 	@Column(name="Name")
-	private String name;
+	private String userName;
 	
 	@Column(name="Email")
 	private String email;
 	
 	@Column(name="Password")
 	private String password;
+	
+	public User(int userID, String userName, String email, String password){
+		this.userId = userID;
+		this.userName = userName;
+		this.email = email;
+		this.password = password;
+	}
+	
+
+	public int getUserId() {
+		return userId;
+	}
+
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	@Override
+	public String toString() {
+		return "User [id=" + userId + ", name=" + userName + ", email=" + email + ", password=" + password + "]";
+	}
 	
 }
