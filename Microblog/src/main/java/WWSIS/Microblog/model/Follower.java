e4bb50e1373e73576701124deaf55d4b1c059691
MykolaHutor1995 <s4389@horyzont.eu>
2020-12-19 09:54:15 +0100

    Follower added

---
+++ Microblog/src/main/java/WWSIS/Microblog/model/Follower.java
@@ -0,0 +1,29 @@
+package WWSIS.Microblog.model; 
+
+import java.sql.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.JoinColumn;
+
+@Entity
+@Table(name="follower")
+public class Follower {
+	@Id
+	@GeneratedValue()
+	@Column(name="FollowerID", nullable=false)
+	private int followerID;
+	public int followerID() {
+		return followerID;
+	}
+	@JoinColumn(name="userId")
+	@Column(name="UserId", nullable=false)
+	private int userId;
+
+	@Column(name="FollowedUserId", nullable=false)
+	private int followedUserId;
+	
+}
