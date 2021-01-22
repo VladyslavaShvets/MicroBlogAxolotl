683a05fc54a5cf17e196aa0f89df2ed20490f1b1
Daria Dziubałtowska <daria305@gmail.com>
2021-01-22 21:42:20 +0100

    Correct Post POJO class

--- Microblog/src/main/java/WWSIS/Microblog/model/Post.java
+++ Microblog/src/main/java/WWSIS/Microblog/model/Post.java
@@ -16,36 +16,57 @@ public class Post {
 	@GeneratedValue()
 	@Column(name="PostId", nullable=false)
 	private int postId;
-	public int getPostId() {
-		return postId;
-	}
 	
 	@JoinColumn(name="userId")
 	@Column(name="UserId", nullable=false)
 	private int userId;
 	
-
 	@Column(name="PostText", nullable=false, length=300)
 	private String postText;
+	
+	@Column(name="Public", nullable=false)
+	private Boolean isPublic;
+	
+	@Column(name="PostedDate", nullable=false)
+	private Date date;
+	
+	public Post(int postId, int userId, String postText, Boolean isPublic, Date date) {
+		this.postId = postId;
+		this.userId = userId;
+		this.postText = postText;
+		this.isPublic = isPublic;
+		this.date = date;
+	}
+	
+	public int getPostId() {
+		return postId;
+	}
+	
+	public int getUserId() {
+		return userId;
+	}
+	
 	public String getPostText() {
 		return postText;
 	}
+	
 	public void setPostText(String text) {
 		this.postText = text;
 	}
 	
-	@Column(name="Public", nullable=false)
-	private Boolean isPublic;
 	public Boolean getIsPublic() {
 		return isPublic;
 	}
+	
 	public void setIsPublic(Boolean isPublic) {
 		this.isPublic = isPublic;
 	}
 	
-	@Column(name="PostedDate", nullable=false)
-	private Date date;
 	public Date getDate() {
 		return this.date;
+	}
+	
+	public void setDate(Date date) {
+		this.date = date;
 	}
 }
