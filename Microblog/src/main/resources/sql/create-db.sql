eff0377b1942ca48c70eee02aa27115591660437
Anita <33136353+anitamiring@users.noreply.github.com>
2021-01-29 15:40:24 +0100

    Update create-db.sql

--- Microblog/src/main/resources/sql/create-db.sql
+++ Microblog/src/main/resources/sql/create-db.sql
@@ -1,25 +1,28 @@
 DROP SCHEMA PUBLIC CASCADE;
 
+drop table user if exists;
 create table user(
     UserId INTEGER IDENTITY NOT NULL PRIMARY KEY,
     UserName nvarchar(55) NOT NULL,
     BirthDate DATE NOT NULL,
     UserPassword nvarchar(30) NOT NULL,
     UserEmail nvarchar(30) NOT NULL,
-
 );
 
+drop table follower if exists;
 create table follower(
 PRIMARY KEY(UserId, FollowedUserId),
 UserId INTEGER FOREIGN KEY  REFERENCES user(UserId),
 FollowedUserId INTEGER FOREIGN KEY REFERENCES user(UserId)
 );
 
-create table post (
+drop table post if exists;
+create table post(
     PostId INTEGER IDENTITY NOT NULL PRIMARY KEY,
-    UserId INTEGER FOREIGN KEY REFERENCES user(UserId) NOT NULL,
+    UserId INTEGER FOREIGN KEY REFERENCES user(UserId),
     PostText nvarchar(300) NOT NULL,
     Public BOOLEAN,
-    PostedDate DATE NOT NULL
+    PostedDate DATE NOT NULL,
 );
+
 
